8d06bda3dcb45ef3a314f63fc059ee3be883a54e
awesomealex85 <alexec206@gmail.com>
2021-05-21 19:49:18 -0700

    updated pupusas

--- course-project/index.js
+++ course-project/index.js
@@ -1,82 +1,44 @@
-//Profile data in JSON format
-const recipeJSON = '[{
-  "name": "Pupusas",
-  "Course": "Front End Developer",
-  "Diet": "Web Weavers",
-  "Prep Time": "3 years",
-  "Cook Time": "University of Washington",
-  "Serving Size": "Marketing",
-  "email": "paolo@example.com",
-  "linkedInUrl": "paolo.linkedinprofile.com",
-  "codeLanguages": ["HTML", "CSS", "JavaScript", "Node", "Express"]
-}]'
-//turns JSON data set into JS object
-const recipe = JSON.parse(recipeJSON)
+//Recipe data in JSON format
+const recipe = [{"image":"pupusas_y_curtido.jpeg", "name": "Pupusas","Course": "Lunch or Dinner","Diet": "gluten free","PrepTime": "30 minutes","CookTime": "10 minutes","ServingSize": "10 people","item1": ["curtido", "2 cups of shredded cabbage", "1 medium carrot, grated", "1 tablespoon dried Mexican oregano", "3/4 cup hot water", "3/4 cup apple cider vinegar", "3/4 teaspoon kosher salt", "3/4 teaspoon granulated sugar"], "item2": ["pupusaDough", "2.25 cup Masa (e.g. Maseca brand)", "1.5 teaspons salt", "2 cups water"], "item3": ["Filling", "1.5 cups quesillo (can use mozzarella)", "3/4 cup Loroco flower (or butternut squash)"]}]
+
+console.log(recipe);
+console.log(recipe.length);
+recipe.forEach(function(el) {
+  console.log(el)
+})
+recipe.forEach(function(el) {
+  console.log(el.item1)
+})
+
 //get section of html to put the card into
 const container = document.querySelector('.template-hook')
 //function for creating card from JSON data
 recipe.forEach(function(el) {
   let list = document.createElement('ul')
   list.innerHTML = `
-  <article class="card">
-		<div class="card__center">
-			<img src="img/headshot.jpg" alt=${el.name}"'s head shot" class="card__img">
+  <article class="recipe">
+		<div class="recipe__center">
+      <!-- <div id = "animated-example" class = ""> -->
+			<img src="img/${el.image}" alt=${el.name}"'s head shot" class="recipe__img animated bounce">
+      </div>
 			<h2>${el.name}</h2>
-			<p><i>${el.jobTitle}</i></p>
+		<!-- </div> -->
+		<div class="recipe__description">
+      <p><strong>Course: </strong><i>${el.Course}</i></p>
+			<p><strong>Diet:</strong> ${el.Diet}</p>
+			<p><strong>Prep Time:</strong> ${el.PrepTime}</p>
+			<p><strong>Cook Time:</strong> ${el.CookTime}</p>
+			<p><strong>Serving Size:</strong> ${el.ServingSize}</p>
 		</div>
-		<div class="card__description">
-			<p><strong>Company:</strong> ${el.company}</p>
-			<p><strong>Expereince:</strong> ${el.expereince}</p>
-			<p><strong>School:</strong> ${el.school}</p>
-			<p><strong>Major:</strong> ${el.major}</p>
-			<p><strong>Email:</strong> ${el.major}</p>
-			<p><img src="img/linkedin.svg" alt="linkedin logo"> ${el.linkedInUrl}</p>
-		</div>
-	</article>
+    <div class="recipe__description">
+      <p><strong>Ingridients:</strong> ${el.item1[0]}</p>
+  </article>
   `
 
-  //
+  // for (let i = 1; i < item1.length; i++) {
+  // const article = document.createElement('p');
+  // p.innerHTML = item1[i];
+  // }
+  //<p>${el.item1.[0]}</p>
   container.append(list)
 })
-
-// Recipe entries
-// const cardJSON = '[{
-//   "name": "Paolo Maldini",
-//   "jobTitle": "Front End Developer",
-//   "company": "Web Weavers",
-//   "experience": "3 years",
-//   "school": "University of Washington",
-//   "major": "Marketing",
-//   "email": "paolo@example.com",
-//   "linkedInUrl": "paolo.linkedinprofile.com",
-// }]'
-// // recipe card generator
-// for (let i = 0; i < profileSet.length; i++) {
-// const article = document.createElement('article');
-// const section = document.querySelector('section.blog');
-// const div = document.createElement('div');
-// const div2 = document.createElement('div');
-// const img = document.createElement('img');
-// img.src = profileSet[i][0];
-//
-// const h2 = document.createElement('h2');
-// const p = document.createElement('p');
-// const p2 = document.createElement('p');
-//
-// section.appendChild(article);
-// article.appendChild(div);
-// article.appendChild(div2);
-// div.appendChild(img);
-//
-// div2.appendChild(h2, p, p2);
-// div2.appendChild(p);
-// div2.appendChild(p2);
-//
-// article.setAttribute("class", "container");
-// div2.setAttribute("class", "blog");
-// img.setAttribute("class", "blog");
-//
-// h2.innerHTML = profileSet[i][1];
-// p.innerHTML = profileSet[i][2];
-// p2.innerHTML = profileSet[i][3];
-// }
